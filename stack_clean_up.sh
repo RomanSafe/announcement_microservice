788e948f51dc3192cff1bc06126a5d8fe627aa3e
Roman Shentsev <3184447@mail.ru>
2021-09-28 20:58:52 +0300

    Simplified stack_clean_up.sh and readme.md.

--- stack_clean_up.sh
+++ stack_clean_up.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -xe
+# Clean up AWS resources: delete CloudFormation stack and S3 bucket
 
-APP_NAME="announcement-app"
+# Print helpFunction in case parameters are empty
+if [ -z "$1" ] || [ -z "$2" ]
+  then
+    echo "Arguments 'stack name' and 'bucket name' are required"
+    exit 1
+fi
 
-aws cloudformation delete-stack --stack-name ${APP_NAME}
+STACK_NAME=$1
+BUCKET_NAME=$2
+
+aws cloudformation delete-stack --stack-name "${STACK_NAME}"
 aws cloudformation wait stack-delete-complete \
---stack-name ${APP_NAME}
-aws s3 rb --force "s3://${APP_NAME}"
+--stack-name "${STACK_NAME}"
+aws s3 rb --force "s3://${BUCKET_NAME}"
